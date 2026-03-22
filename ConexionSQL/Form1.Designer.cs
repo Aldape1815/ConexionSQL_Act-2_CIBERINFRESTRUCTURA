550f6be414e21e2f13f08bf0937f2bd946ad01f9
Aldape18 <103906163+Aldape1815@users.noreply.github.com>
2026-03-22 13:51:01 -0600

    Agregar archivos de proyecto.

---
+++ ConexionSQL/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace ConexionSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textUsuario = new System.Windows.Forms.TextBox();
+            this.textContraseña = new System.Windows.Forms.TextBox();
+            this.textHost = new System.Windows.Forms.TextBox();
+            this.textBD = new System.Windows.Forms.TextBox();
+            this.textPuerto = new System.Windows.Forms.TextBox();
+            this.btnConexion = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.DgvDatos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textUsuario
+            // 
+            this.textUsuario.Location = new System.Drawing.Point(116, 103);
+            this.textUsuario.Name = "textUsuario";
+            this.textUsuario.Size = new System.Drawing.Size(183, 20);
+            this.textUsuario.TabIndex = 0;
+            // 
+            // textContraseña
+            // 
+            this.textContraseña.Location = new System.Drawing.Point(116, 129);
+            this.textContraseña.Name = "textContraseña";
+            this.textContraseña.Size = new System.Drawing.Size(183, 20);
+            this.textContraseña.TabIndex = 1;
+            // 
+            // textHost
+            // 
+            this.textHost.Location = new System.Drawing.Point(116, 155);
+            this.textHost.Name = "textHost";
+            this.textHost.Size = new System.Drawing.Size(183, 20);
+            this.textHost.TabIndex = 2;
+            // 
+            // textBD
+            // 
+            this.textBD.Location = new System.Drawing.Point(116, 181);
+            this.textBD.Name = "textBD";
+            this.textBD.Size = new System.Drawing.Size(183, 20);
+            this.textBD.TabIndex = 3;
+            // 
+            // textPuerto
+            // 
+            this.textPuerto.Location = new System.Drawing.Point(116, 207);
+            this.textPuerto.Name = "textPuerto";
+            this.textPuerto.Size = new System.Drawing.Size(183, 20);
+            this.textPuerto.TabIndex = 4;
+            // 
+            // btnConexion
+            // 
+            this.btnConexion.Location = new System.Drawing.Point(116, 266);
+            this.btnConexion.Name = "btnConexion";
+            this.btnConexion.Size = new System.Drawing.Size(176, 62);
+            this.btnConexion.TabIndex = 5;
+            this.btnConexion.Text = "Conectar";
+            this.btnConexion.UseVisualStyleBackColor = true;
+            this.btnConexion.Click += new System.EventHandler(this.btnConexion_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Javanese Text", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(206, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 47);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Conexion Grafica MySQL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Contraseña";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(81, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Host";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(88, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "BD";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(69, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Puertro";
+            // 
+            // DgvDatos
+            // 
+            this.DgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvDatos.Location = new System.Drawing.Point(334, 103);
+            this.DgvDatos.Name = "DgvDatos";
+            this.DgvDatos.Size = new System.Drawing.Size(325, 225);
+            this.DgvDatos.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 365);
+            this.Controls.Add(this.DgvDatos);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConexion);
+            this.Controls.Add(this.textPuerto);
+            this.Controls.Add(this.textBD);
+            this.Controls.Add(this.textHost);
+            this.Controls.Add(this.textContraseña);
+            this.Controls.Add(this.textUsuario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textUsuario;
+        private System.Windows.Forms.TextBox textContraseña;
+        private System.Windows.Forms.TextBox textHost;
+        private System.Windows.Forms.TextBox textBD;
+        private System.Windows.Forms.TextBox textPuerto;
+        private System.Windows.Forms.Button btnConexion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView DgvDatos;
+    }
+}
+
---
+++ ConexionSQL/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using MySql.Data.MySqlClient;
+
+namespace ConexionSQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Funcion de conexion al hacer click en el boton, donde se toman los datos ingresados por el
+        //usuario para establecer la conexion con la base de datos y mostrar los datos de la tabla
+        //CatPersonal en el DataGridView
+        private void btnConexion_Click(object sender, EventArgs e)
+        {
+            // Conversion de los datos ingresados a tipo String para futuras referencias
+            string host = textHost.Text;
+            string user = textUsuario.Text;
+            string password = textContraseña.Text;
+            string puerto = textPuerto.Text;
+            string database = textBD.Text;
+
+            // Sentencia para establecer conexion con la BD, pasando los atributos necesarios
+            string connectionString = BuildConnectionString(host, user, password, puerto, database);
+
+            //Funcion de conexion a la base de datos, usando el string de conexion construido
+            //previamente, y mostrando los datos de la tabla CatPersonal en el DataGridView
+            using (MySqlConnection con = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM CatPersonal", con);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    DgvDatos.DataSource = dt;
+                    // Sentencia para lograr la visualizacion de las filas de la tabla CatPersonal
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show("Ha ocurrido un error: " + ex.Message);
+
+                }
+                // Mensaje de error, en caso de no poder establecer conexion y mostrar los datos en el grid.
+            }
+        }
+
+        // Sentencia que establece la conexion con la base de datos, usando como parametros los datos
+        // ingresados por el usuario
+        private string BuildConnectionString(string host, string user, string password, string puerto, string database)
+        {
+            return $"SERVER={host};PORT={puerto};DATABASE={database};USER ID={user};PASSWORD={password};";
+        }
+        
+    }
+}
